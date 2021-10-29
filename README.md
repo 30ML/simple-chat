88ed45f4ab96521e300538be94ebacd62a2018cf
Min Jae Kim <exploit017@gmail.com>
2021-10-29 15:58:37 +0900

    코드 수정

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Discord_Chatbot
+# Discord_Bot
+봇으로 로그인하고 사용자 입력에 댓글을 다는 챗봇
 * node.js: v.16.13.0
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const Discord = require('discord.js');
 const dotenv = require('dotenv');
-dotenv.config({ path: './.env' });
 
-const client = new Discord.Client({intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES]});
+dotenv.config({ path: './.env' });
+const { BOT_TOKEN } = process.env;
+
+const client = new Discord.Client({
+  intents: [
+    Discord.Intents.FLAGS.GUILDS,
+    Discord.Intents.FLAGS.GUILD_MESSAGES],
+});
 
 client.once('ready', () => {
   console.log('Bot is ready!');
@@ -14,4 +20,4 @@ client.on('messageCreate', message => {
   }
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login(BOT_TOKEN);
